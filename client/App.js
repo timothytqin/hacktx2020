4a43290847762db4a5d16aab040ee357fc0e7b3e
Steve Han <stevehan2001@gmail.com>
2020-10-25 02:48:17 -0500

    added more data

--- client/App.js
+++ client/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Home from './screens/RootDrawerNav';
 import Auth from './screens/AuthStackNav';
 
-const AuthContext = React.createContext({ user: null, setUser: null });
+const AuthContext = React.createContext({
+  user: null,
+  setUser: null,
+  properties: [],
+  setProperties: null,
+});
+
+const properties = require('./data/properties.json');
 export default function App() {
   const [user, setUser] = React.useState(null);
+  const [properties, setProperties] = React.useState([]);
+  useEffect(() => {
+    setProperties(properties);
+  }, []);
   return (
-    <AuthContext.Provider value={{ user: user, setUser: setUser }}>
+    <AuthContext.Provider
+      value={{ user: user, setUser: setUser, properties, setProperties }}
+    >
       <NavigationContainer>{!user ? <Auth /> : <Home />}</NavigationContainer>
     </AuthContext.Provider>
   );
