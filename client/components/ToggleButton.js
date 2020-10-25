c089b13615faf735916556bc4b5ce836beb27b60
Timothy Qin <timothy.t.qin@gmail.com>
2020-10-25 02:45:44 -0500

    finished create listing

---
+++ client/components/ToggleButton.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
+
+export default function ToggleButton() {
+  const [selected, setSelected] = useState(-1);
+  return <View></View>;
+}
